ca8cc33225a2ad6f31dbfe1944ff1caa0b9dc01e
haoqi75 <haoqi071203@gmail.com>
2026-08-11 14:14:25 +0200

    把详细介绍补充

--- src/customContent.ts
+++ src/customContent.ts
@@ -3,5 +3,14 @@
 export const customContentHTML = `
   <h2>欢迎来到 ApHeQua758</h2>
   <img src="https://github-stats-extended.vercel.app/api?username=haoqi75&rank_icon=percentile&custom_title=ApHeQua758%E7%9A%84%E4%B8%AA%E4%BA%BA%E7%8A%B6%E6%80%81&show_icons=true&include_all_commits=true&theme=shadow_red&locale=cn" alt="GitHub Stats">
-  <p>..后续填充..</p>
+  <h3>介绍一下</h3>
+  <p>哈咯！开发作者们，我是ApHeQua758（中文名啊非刮至758），这虽然不是我的真正名字，但是也可以了。</p>
+  <p>我对于开发程序还在基础，但对于认识语言以及差不多了，对于我这种喜欢电脑的人来说，开发部分软件还会的。</p>
+  <p>我经常使用AI用来开发有些代码，做的更美化一些。我偶尔不使用AI修复代码，因为我自己会修。</p>
+  <p>对于Moe我要是一点点喜欢，因此我的某些作品用Moe样式的。</p>
+  <h3>网页状态页面</h3>
+  <a href="https://haoqi75.cn.mt"><img src="https://img.shields.io/website?url=https%3A%2F%2Fhaoqi75.cn.mt&up_message=%E5%9C%A8%E7%BA%BF&up_color=cyan&down_message=%E7%A6%BB%E7%BA%BF&down_color=grey&style=for-the-badge&logo=Cloudflare&logoColor=white&label=haoqi75.cn.mt&labelColor=red" alt="状态页面链接"></a>
+  <h3>连接</h3>
+  <a href="https://haoqi75.os.kg"><img src="https://img.shields.io/endpoint?url=https%3A%2F%2Fhaoqi75.cn.mt%2Fapi%2Fbadge%3Fid%3Dmy_page%26label%3D%25E9%25A6%2596%25E9%25A1%25B5%26up%3D%25E5%259C%25A8%25E7%25BA%25BF%26down%3D%25E7%25A6%25BB%25E7%25BA%25BF%26colorUp%3Dcyan%26colorDown%3Dgrey&style=for-the-badge&logoColor=white&labelColor=red" alt="首页"></a>
+  <a href="https://haoqi75.qd.je"><img src="https://img.shields.io/endpoint?url=https%3A%2F%2Fhaoqi75.cn.mt%2Fapi%2Fbadge%3Fid%3Dmy_blog%26label%3D%25E5%258D%259A%25E5%25AE%25A2%26up%3D%25E5%259C%25A8%25E7%25BA%25BF%26down%3D%25E7%25A6%25BB%25E7%25BA%25BF%26colorUp%3Dcyan%26colorDown%3Dgrey&style=for-the-badge&logoColor=white&labelColor=red" alt="博客"></a>
 `;
